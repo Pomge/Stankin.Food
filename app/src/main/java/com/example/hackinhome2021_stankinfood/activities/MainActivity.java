f3c3ff3a37a7806960c16e32532446026bd3d878
Aleksandr <aleks.berkovets@yandex.ru>
2021-04-23 19:53:19 +0300

    Created MyRecyclerViewAdapter

--- app/src/main/java/com/example/hackinhome2021_stankinfood/activities/MainActivity.java
+++ app/src/main/java/com/example/hackinhome2021_stankinfood/activities/MainActivity.java
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hackinhome2021_stankinfood.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    public static final int MENU_HEADER = 0;
+    public static final int MENU_PRODUCT_INACTIVE = 1;
+    public static final int MENU_PRODUCT_ACTIVE = 2;
+    public static final int ORDER_PRODUCT_INACTIVE = 3;
+    public static final int ORDER_PRODUCT_ACTIVE = 4;
 
     private static final String COLLECTION_RESTAURANTS = "restaurants";
     private static final String COLLECTION_ORDERS = "orders";
     private static final String COLLECTION_PRODUCTS = "products";
     private static final String COLLECTION_FAVORITE_ORDERS = "favoriteOrders";
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/hackinhome2021_stankinfood/adapters/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/hackinhome2021_stankinfood/adapters/MyRecyclerViewAdapter.java
@@ -1,4 +1,56 @@
 package com.example.hackinhome2021_stankinfood.adapters;
 
-public class MyRecyclerViewAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+        implements Filterable {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    private static class ViewHolderMenuHeader extends RecyclerView.ViewHolder {
+
+        public ViewHolderMenuHeader(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    private static class ViewHolderMenuProduct extends RecyclerView.ViewHolder {
+
+        public ViewHolderMenuProduct(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    private static class ViewHolderOrderProduct extends RecyclerView.ViewHolder {
+
+        public ViewHolderOrderProduct(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
