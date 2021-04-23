73556df25e2661b04d18c08646ed2ef8ab852cb3
PomaPlayer89 <p0mazannatasha@yandex.ru>
2021-04-23 22:29:38 +0300

    OrderFragment to fragment

--- app/src/main/java/com/example/hackinhome2021_stankinfood/fragments/OrderFragment.java
+++ app/src/main/java/com/example/hackinhome2021_stankinfood/fragments/OrderFragment.java
@@ -1,4 +1,4 @@
-package com.example.hackinhome2021_stankinfood;
+package com.example.hackinhome2021_stankinfood.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.hackinhome2021_stankinfood.R;
 
 /**
  * A simple {@link Fragment} subclass.
