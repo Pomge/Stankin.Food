55138a1ab50c5d53988330a7c59ca0e7f324276f
AliceNoybert <81040950+AliceNoybert@users.noreply.github.com>
2021-04-24 01:47:02 +0300

    LayoutCart3.0

--- app/src/main/java/com/example/hackinhome2021_stankinfood/fragments/CartFragment.java
+++ app/src/main/java/com/example/hackinhome2021_stankinfood/fragments/CartFragment.java
@@ -7,27 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.hackinhome2021_stankinfood.R;
 
 
 public class CartFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String KEY_CART = "cart";
 
-    private String mParam1;
-    private String mParam2;
+    private String cart;
+    private TextView textViewRealAddress;
+    private TextView textViewPickupTime;
+    private TextView textViewRealPickupTime;
+    private TextView textViewRealTotalPrice;
 
     public CartFragment() {
         // Required empty public constructor
     }
 
-    public static CartFragment newInstance(String param1, String param2) {
+    public static CartFragment newInstance(String cart) {
         CartFragment fragment = new CartFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(KEY_CART, cart);
         fragment.setArguments(args);
         return fragment;
     }
@@ -36,15 +38,15 @@ public class CartFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            cart = getArguments().getString(KEY_CART);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        return view;
     }
 }
