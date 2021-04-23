a951f10727083b5fa555682e63b207e130ab4e09
Aleksandr <aleks.berkovets@yandex.ru>
2021-04-23 19:34:52 +0300

    Filled POJO classes

--- app/src/main/java/com/example/hackinhome2021_stankinfood/activities/MainActivity.java
+++ app/src/main/java/com/example/hackinhome2021_stankinfood/activities/MainActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import com.example.hackinhome2021_stankinfood.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String COLLECTION_RESTAURANTS = "restaurants";
+    private static final String COLLECTION_ORDERS = "orders";
+    private static final String COLLECTION_PRODUCTS = "products";
+    private static final String COLLECTION_FAVORITE_ORDERS = "favoriteOrders";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
